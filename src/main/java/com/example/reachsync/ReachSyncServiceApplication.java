8a000ad99e0fc50d9e38cedd748b92b446b3cea8
JuhiNayak <jyc@Juhis-Laptop.local>
2026-03-06 17:58:46 -0800

    Initial commit

---
+++ src/main/java/com/example/reachsync/ReachSyncServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reachsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachSyncServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReachSyncServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reachsync/ReachSyncServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reachsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReachSyncServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
